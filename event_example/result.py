34150a7718c77bf5583b36e5d6528d3729acb0aa
Christopher Angell <ctangell13@gmail.com>
2022-02-21 23:17:18 +0900

    add comments and doc strings

--- event_example/result.py
+++ event_example/result.py
@@ -2,48 +2,69 @@
 # All rights reserved
 # This software is licensed under the terms of the LGPL included in LICENSE.txt
 
-from tkinter import IntVar, StringVar, Toplevel, ttk
+from tkinter import StringVar, ttk
 
 from .events import FetchResultEvent, UpdateEvent
 
 class ResultWindow:
+    """ Window to display a result.  Allows modification of the result and
+    posting the update to the database by means of the event bus.
+    """
 
     def __init__(self, root, eventbus):
+        # save the event bus so events can be emitted later
         self._eventbus = eventbus
 
+        # This window is the main window, so just use the root display
         t = root
 
+        # add several labels for the result fields
+        # 'grid' is necessary to get an item to actually display
         ttk.Label(t, text="Name").grid(column=0, row=0)
         ttk.Label(t, text="Color").grid(column=0, row=1)
         ttk.Label(t, text="Quantity").grid(column=0, row=2)
 
+        # create display/edit fields for each of the result fields
+        # the StringVar allows synching with the display
         self._name = StringVar()
         ttk.Label(t, textvariable=self._name).grid(column=1, row=0)
         self._color = StringVar()
         ttk.Entry(t, textvariable=self._color).grid(column=1, row=1)
         self._quantity = StringVar()
         ttk.Entry(t, textvariable=self._quantity).grid(column=1, row=2)
+        # add a button to trigger an update
         button = ttk.Button(t, text="Update", command=self.update)
         button.grid(column=0, row=3, columnspan=2)
+        # show validation error in case data is incorrect (quantity not int)
         self._error = StringVar()
         errorlabel = ttk.Label(t, textvariable=self._error)
         errorlabel.grid(column=0, row=4, columnspan=2)
 
+        # connect the 'receive' function to handle the FetchResultEvent from db
         eventbus.connect(FetchResultEvent, self.receive)
 
     def receive(self, event):
+        # event is FetchResultEvent
         self._name.set(event.data.name)
         self._color.set(event.data.color)
         self._quantity.set(str(event.data.quantity))
 
     def update(self, *args):
+        # perform an update of the data
         try:
+            # validate the entries first
             quantity = int(self._quantity.get())
         except ValueError:
             self._error.set("Please enter int for quantity")
             return
 
+        # incase successful validation, clear the error
         self._error.set("")
+        # fetch the other data from the display
         name = self._name.get()
         color = self._color.get()
+        # trigger an update of the data
+        # note: this doesn't block to prevent multiple updates while previous
+        # updates are processing, but in production you would want to disable
+        # the button until saving is done or another result is loaded
         self._eventbus.emit(UpdateEvent([name, color, quantity]))
--- event_example/search.py
+++ event_example/search.py
@@ -7,35 +7,56 @@ from tkinter import Listbox, StringVar, Toplevel, ttk
 from .events import FetchEvent, SearchEvent, SearchResultEvent
 
 class SearchWindow:
+    """ Window to conduct a search, and peruse abbreviated results. 
+    Allows for selecting a result, and coordinating with the database and the
+    result window through the event bus to load the result and display in result
+    window.
+    """
 
     def __init__(self, root, eventbus):
+        # save the _eventbus as we need this to emit events later
         self._eventbus = eventbus
 
+        # make a top level window
         t = Toplevel(root)
         
+        # add a label to box, the grid is required to get the item to show
         ttk.Label(t, text="Query").grid(column=0, row=0)
 
+        # a string var synchronizes with the display, entered value will be here
         self._query = StringVar()
+        # add a text entry to the display
         ttk.Entry(t, textvariable=self._query).grid(column=1, row=0)
 
+        # add a button to search, will call 'submit' when clicked
         button = ttk.Button(t, text="Search", command=self.submit)
         button.grid(column=2, row=0, sticky='nwes')
 
-        
+        # multi-line text fields also use StringVar
         self._results = StringVar()
+        # create the list box, and show 5 items in it
         self._listbox = Listbox(t, listvariable=self._results, height=5)
         self._listbox.grid(column=0, row=1, columnspan=3, sticky='nwes')
+        # the bind is necessary to react on a click in the list box
         self._listbox.bind('<<ListboxSelect>>', self.fetch)
 
+        # register the 'receive' method as a handler for SearchResultEvent
         eventbus.connect(SearchResultEvent, self.receive)
 
     def submit(self, *args):
+        # Conduct a search for the given query.
+        # Search result will show up the listbox (see 'receive' below)
         self._eventbus.emit(SearchEvent(self._query.get()))
 
     def receive(self, event):
+        # event is a SearchResultEvent
+        # set the display to the search result
         self._results.set(event.data)
 
     def fetch(self, *args):
+        # When an item is selected from the listbox, send an event to fetch the
+        # full item.
+        # The full item will show up in the result window. (see result.py)
         try:
             index = self._listbox.curselection()[0]
         except IndexError:
